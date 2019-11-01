acdde396477633b50dddb84a7467a0b78de1c1be
Abisai Rodriguez <abisai.rodriguez13@gmail.com>
2019-11-01 16:33:34 -0700

    created the search.ejs file in views folder

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express();
 const request = require("request");
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res){
+    res.render("search")
+});
+
 app.get("/results", function(req, res){
     request("http://www.omdbapi.com/?s=iowa&apikey=thewdb", function(error, response, body){
         if( !error && response.statusCode == 200){
