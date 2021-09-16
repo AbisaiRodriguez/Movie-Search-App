9f36972660ec807a3e3b6301ed254055fa808392
Abisai Rodriguez <abisai.rodriguez13@gmail.com>
2021-09-16 14:24:07 -0700

    css,images and error

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const request = require("request");
+app.use(express.static(__dirname + '/public'));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
@@ -11,13 +12,19 @@ app.get("/results", function(req, res){
     const query = req.query.search;
     const url = "http://www.omdbapi.com/?s=" + query + "&apikey=thewdb";
     request( url, function(error, response, body){
-        if( !error && response.statusCode == 200){
-            const parseData = JSON.parse(body);
-            res.render("results", {parseData: parseData});
-        }
+    
+            if( !error && response.statusCode == 200){
+                const parseData = JSON.parse(body);
+                console.log(parseData);
+                if( parseData.Response == "False"){
+                    res.render("search");
+                }
+                res.render("results", {parseData: parseData});
+            }
+        
     })
 });
 
-//app.listen(3000);
-app.listen(process.env.PORT, process.env.IP);
+app.listen(3000);
+//app.listen(process.env.PORT, process.env.IP);
 console.log("Movie app has Started");
